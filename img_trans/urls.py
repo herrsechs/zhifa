070d6713638d5cc406eb360f2c55c6dd2c737dc3
herrsechs <herrsechs@foxmail.com>
2016-08-10 22:50:11 +0800

    Fix image downloading func
    Add url in img_trans/urls.py

--- img_trans/urls.py
+++ img_trans/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     url(r'^upload_hair_img/.*$', views.upload_hair_img, name='upload_hair_img'),
     url(r'^upload_head_img/.*$', views.upload_head_img, name='upload_head_img'),
     url(r'^upload_selfie_img/.*$', views.upload_selfie_img, name='upload_selfie_img'),
-    url(r'^get_hair_img/.*$', views.get_hair_img, name='get_hair_img')
+    url(r'^get_hair_img/.*$', views.get_hair_img, name='get_hair_img'),
+    url(r'^get_head_img/.*$', views.get_head_img, name='get_head_img'),
+    url(r'^get_selfie_img/.*$', views.get_selfie_img, name='get_selfie_img')
 ]

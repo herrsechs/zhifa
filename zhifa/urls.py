f512dad6d8d867a4bc1a4c964ca35f9a58ce656f
Liu Yajun <herrsechs@foxmail.co>
2017-04-04 16:13:08 +0800

    Add static file url patterns

--- zhifa/urls.py
+++ zhifa/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^img/', include('img_trans.urls')),
@@ -24,3 +25,4 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^zhifa-admin/', include('zhifa-admin.urls'))
 ]
+urlpatterns += staticfiles_urlpatterns()
